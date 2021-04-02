833989a4702aa661461bb684abda97175a51d6bd
iruhama <81814267+iruhama@users.noreply.github.com>
2021-04-02 20:17:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'AS-BOT'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN: IRUHAMA\n' // Nama kamu
+            + 'ORG: ANIME SOCIETY;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=085210389557\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
